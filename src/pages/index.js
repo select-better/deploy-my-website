58769b873ace1655f17db0882a1f4799d5b2e987
wangchaoli <wangchaoli@enn.cn>
2023-03-01 19:25:30 +0800

    feat: 修改数据

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            test - welcome - 5min ⏱️
+            test - wcl - 5min ⏱️
           </Link>
         </div>
       </div>
